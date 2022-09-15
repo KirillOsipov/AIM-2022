65025c743648bd51094716ce64f34ab629474e61
KirillOsipov <62734616+KirillOsipov@users.noreply.github.com>
2022-09-15 19:11:05 +0400

    add_models

---
+++ Lab_1/Project_DB/Models/Authentication.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OsipovCourseWork.Models
+{
+    public class LoginModel
+    {
+        [Display(Name = "Логин")]
+        public string Login { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Придумайте логин.")]
+        [Display(Name = "Логин")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Придумайте пароль.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение пароля")]
+        [Compare("Password", ErrorMessage = "Пароль и его подтверждение не совпадают.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
